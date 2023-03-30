bd80808d80bd425ed160558be47be673846c16d6
MetthewOlsen <metthew_74@mail.ru>
2023-03-30 20:00:49 +0300

    вторая задача

--- Seminar3/Program.cs
+++ Seminar3/Program.cs
@@ -21,3 +21,31 @@
 // if (Palindrom(x) == true)
 //     Console.WriteLine("Число является палиндромом.");
 // else Console.WriteLine("Число не является палиндромом.");
+
+//Задача 21. Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+// double Length(int x1, int y1, int z1, int x2, int y2, int z2)
+// {
+//     return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+// }
+
+// Console.Write("Введите x точки А: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите у точки А: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z точки А: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine();
+
+// Console.Write("Введите x точки B: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите у точки B: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите z точки B: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine();
+
+// Console.WriteLine($"Расстояние между точками А и В равно {Math.Round(Length(x1, y1, z1, x2, y2, z2), 2)}");
