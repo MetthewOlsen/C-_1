fdb48b7e34f0c7e914262933cc47caf5622032c5
MetthewOlsen <metthew_74@mail.ru>
2023-03-30 19:58:36 +0300

    первая задача

---
+++ Seminar3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// bool Palindrom(int n)
+// {
+//     int nConst = n;
+//     int nNew = 0;
+//     while (n > 0)
+//     {
+//         int lastDigit = n % 10;
+//         nNew = nNew * 10 + lastDigit;
+//         n = n / 10;
+//     }
+//     if (nNew == nConst)
+//         return true;
+//     else return false;
+// }
+
+// Console.WriteLine("Введите любое целое число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// if (Palindrom(x) == true)
+//     Console.WriteLine("Число является палиндромом.");
+// else Console.WriteLine("Число не является палиндромом.");
